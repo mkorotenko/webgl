d3d1e8e7dc56c8378be9878b8d160ea47bd11f95
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-05-09 00:45:39 +0300

    webpack setup

--- src/index.js
+++ src/index.js
@@ -1,26 +1,131 @@
-// import THREE from 'three';
+import THREE from './three';
+
+console.info('THREE', THREE)
+
+function updateGroupGeometry( mesh, geometry ) {
+
+	mesh.children[ 0 ].geometry.dispose();
+	mesh.children[ 1 ].geometry.dispose();
+
+	mesh.children[ 0 ].geometry = new THREE.WireframeGeometry( geometry );
+	mesh.children[ 1 ].geometry = geometry;
+
+	// these do not update nicely together if shared
+
+}
+
+const BoxBufferGeometry = function ( mesh ) {
+
+    var data = {
+        width: 15,
+        height: 15,
+        depth: 15,
+        widthSegments: 1,
+        heightSegments: 1,
+        depthSegments: 1
+    };
+
+    function generateGeometry() {
+
+        updateGroupGeometry( mesh,
+            new THREE.BoxBufferGeometry(
+                data.width, data.height, data.depth, data.widthSegments, data.heightSegments, data.depthSegments
+            )
+        );
+
+    }
+
+    generateGeometry();
+
+}
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 50 );
+camera.position.z = 30;
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( 0x000000, 1 );
 document.body.appendChild( renderer.domElement );
+// var renderer = new THREE.WebGLRenderer({ antialias: true });
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var orbit = new THREE.OrbitControls( camera, renderer.domElement );
+orbit.enablezoom = false;
+// new THREE.FirstPersonControls(camera, renderer.domElement);
 
-camera.position.z = 5;
+var lights = [];
+lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+// lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+// lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 
-var animate = function () {
-    requestAnimationFrame( animate );
+lights[ 0 ].position.set( 0, 200, 0 );
+// lights[ 1 ].position.set( 100, 200, 100 );
+// lights[ 2 ].position.set( - 100, - 200, - 100 );
 
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+scene.add( lights[ 0 ] );
+// scene.add( lights[ 1 ] );
+// scene.add( lights[ 2 ] );
 
-    renderer.render(scene, camera);
+var mesh = new THREE.Object3D();
+
+mesh.add( new THREE.LineSegments(
+
+    new THREE.Geometry(),
+
+    new THREE.LineBasicMaterial( {
+        color: 0xffffff,
+        transparent: true,
+        opacity: 0.5
+    } )
+
+) );
+
+mesh.add( new THREE.Mesh(
+
+    new THREE.Geometry(),
+
+    new THREE.MeshPhongMaterial( {
+        color: 0x156289,
+        emissive: 0x072534,
+        side: THREE.DoubleSide,
+        flatShading: true
+    } )
+
+) );
+
+//var options = chooseFromHash( mesh );
+BoxBufferGeometry(mesh);
+
+scene.add( mesh );
+
+var prevFog = false;
+
+var render = function () {
+
+    requestAnimationFrame( render );
+
+    //if ( ! options.fixed ) {
+
+        mesh.rotation.x += 0.005;
+        mesh.rotation.y += 0.005;
+
+    //}
+
+    renderer.render( scene, camera );
+
 };
 
-animate();
+window.addEventListener( 'resize', function () {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}, false );
+
+render();
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'common.bundle.js'
+    },
+    plugins: [
+        //new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader"
+            }
+          }
+        ]
     }
-}
+};
